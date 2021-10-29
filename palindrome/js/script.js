65214ae863d60a0064a35e51913ad4573e02efcd
Mario <lombardomario01@gmail.com>
2021-10-29 13:21:19 +0200

    Created files

---
+++ palindrome/js/script.js
@@ -0,0 +1,15 @@
+// -- PALINDROMA
+
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma.
+Dare l’output relativo.
+*/
+
+/*
+
+1.
+
+*/
+
+console.log("Eccomi, sono Palindroma!!");
